faacf08b3e92689af0e2f50117364f8298cb2af1
Lindsay Sawicki <100655577+lindsaysawicki@users.noreply.github.com>
2022-04-26 10:55:37 -0500

    app.py add

--- app.py
+++ app.py
@@ -1,57 +1,62 @@
+from crypt import methods
 from flask import Flask
-from flask import render_template, redirect, request, url_for
-from datetime import date
+from flask import render_template, redirect,request,url_for
 
 app = Flask(__name__)
 
-birth_year = 0
-current_age = 0
+radius = 0
+height = 0
 
-friend_list = [{"name": "Mike Colbert" } ]
+top_area = 3.14 * (radius^2)
+side_area = (2)*(3.14)*(height*radius)
+total_area = top_area+side_area
+total_area_sq_ft = total_area/(144)
+material_cost = total_area_sq_ft*(25)
+labor_cost = total_area_sq_ft*(15)
+total_cost = material_cost+labor_cost
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Mike\'s Friends', friends = friend_list)
+    return render_template('index.html')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html', pageTitle='About Mike')
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle="About")
 
-def calculate_current_age(birth_year):
-    today = date.today()
-    return today.year - float(birth_year)
 
-def calculate_future_age(current_age):
-    return current_age + 10
+def calculate_total_area(total_cost):
+    top_area = 3.14 * (radius^2)
+    side_area = (2)*(3.14)*(height*radius)
+    total_area = top_area+side_area
+    total_area_sq_ft = total_area/(144)
+    total_cost= total_area_sq_ft*(25)+total_area_sq_ft*(15)  
+    return float(total_cost)
 
-def calculate_past_age(current_age):
-    return current_age - 10
+def calc_rad(radius):
+    return float(radius)
 
-@app.route('/age', methods=['GET', 'POST'])
-def age():
+def calc_height(height):
+    return float(height)
+
+@app.route('/estimate_info', methods=['GET','POST'])
+def estimate():
     if request.method == 'POST':
-        #capture year from form:
+        #capture height & radius from form:
         form = request.form
-        birth_year = form['birth_year']
-        current_age = calculate_current_age(birth_year)
-        if 'future_age' in request.form:
-            age = calculate_future_age(current_age)
-        if 'past_age' in request.form:
-            age = calculate_past_age(current_age)
-        return render_template('age.html', pageTitle='Calculate Your Age', age=age)
-    return render_template('age.html', pageTitle='Calculate Your Age')
+        radius = form['radius']
+        height = form['height']
+        radius= calc_rad(radius)
+        height=calc_height(height)
+        if 'radius' in request.form:
+            age=calculate_total_area(total_cost)
+        if 'height' in request.form:
+            age=calculate_total_area(total_cost)
+        return render_template('estimate_info.html', pageTitle='Calculate Your estimate', age=age)
+    return render_template('estimate_info.html', pageTitle='Calculate Your estimate')
 
-@app.route('/add_friend', methods=['GET', 'POST'])
-def add_friend():
-    if request.method == 'POST':
-        form = request.form
-        fname = form['fname']
-        lname = form['lname']
-        friend_dict = {"name": fname + " " + lname}
-        friend_list.append(friend_dict)
-        return redirect(url_for('index'))
-    return redirect(url_for('index'))
-    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
